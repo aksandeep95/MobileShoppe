6a286858e6e71d76b8ea07006cf925355cd44584
aksandeep95 <sankriann@gmail.com>
2021-02-09 18:15:23 +0530

    first commit

---
+++ WebPage11/Controllers/MobileController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPage11.Models;
+using System.Data.Entity;
+
+namespace WebPage11.Controllers
+{
+    public class MobileController : Controller
+    {
+
+        DBaseEntities1 dBaseEntities1 = new DBaseEntities1();
+
+        // GET: Mobile
+        
+        public ActionResult showmobile()
+        {
+
+           var mobilelist =  dBaseEntities1.mobiles.ToList();
+            return View(mobilelist);
+        }
+        
+        public ActionResult Addmobile()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Addmobile(mobile mobiledata)
+        {
+
+            dBaseEntities1.Entry(mobiledata).State = EntityState.Added;
+            dBaseEntities1.SaveChanges();
+            return RedirectToAction("showmobile");
+        }
+
+        public ActionResult editmobile(int id)
+        {
+            var mobiledata = dBaseEntities1.mobiles.Find(id);
+            return View(mobiledata);
+        }
+
+        [HttpPost]
+        public ActionResult editmobile(mobile mobiledata)
+        {
+            dBaseEntities1.Entry(mobiledata).State = EntityState.Modified;
+            dBaseEntities1.SaveChanges();
+            return RedirectToAction("showmobile");
+        }
+       
+        public ActionResult deletemobile(int id)
+        {
+
+            var mobiledata = dBaseEntities1.mobiles.Find(id);
+            dBaseEntities1.Entry(mobiledata).State = EntityState.Deleted;
+            dBaseEntities1.SaveChanges();
+            return RedirectToAction("showmobile");
+        }
+    }
+}
---
+++ WebPage11/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebPage11.MvcApplication" Language="C#" %>
